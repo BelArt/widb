e724346d85659beba10ef0f4fa94f902c9cf76ab
Alexey Snigirev <snigirev.alexey@gmail.com>
2013-12-07 14:27:42 +0400

    Поправлено построение списка возможных родительских коллекций при создании/редактировании коллекции

--- protected/views/collections/_formTempCollection.php
+++ protected/views/collections/_formTempCollection.php
@@ -37,7 +37,7 @@ echo CHtml::openTag('div', array(
 echo $form->select2Row($model,'parent_id',
     array(
         'asDropDownList' => true,
-        'data' => $model->getArrayOfPossibleParentCollections(),
+        'data' => $model->getArrayOfPossibleTempParentCollections(),
         'class' => 'input-xxlarge _collectionForm_parentCollectionSelect',
     )
 );
