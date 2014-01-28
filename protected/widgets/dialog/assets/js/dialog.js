8e1f79ba6cb0332200b35dee232783ec5eab3798
Alexey Snigirev <snigirev.alexey@gmail.com>
2014-01-28 13:02:19 +0400

    Виджет диалога

---
+++ protected/widgets/dialog/assets/js/dialog.js
@@ -0,0 +1,51 @@
+/**
+ * Показывает диаолговой окно. Используется jquery ui плагин dialog
+ * @param title заголовок
+ * @param msg сообщение
+ * @param okButtonCallback колбэк на нажатие кнопки ОК
+ * @param cancelButtonCallback колбэк на нажатие кнопки Отмена
+ */
+function showDialog(title, msg, okButtonCallback, cancelButtonCallback) {
+
+    var $dialog = $('._dialog');
+
+    title = title || '';
+    msg = msg || '';
+    okButtonCallback = okButtonCallback || null;
+    cancelButtonCallback = cancelButtonCallback || null;
+
+    var buttons = {};
+    if (okButtonCallback) {
+        buttons[$dialog.data('button-ok')] = okButtonCallback;
+    }
+    if (cancelButtonCallback) {
+        buttons[$dialog.data('button-cancel')] = cancelButtonCallback;
+    }
+
+    $dialog.html(msg);
+
+    $dialog.dialog({
+        title: title,
+        modal: true,
+        resizable: false,
+        buttons: buttons
+    });
+}
+
+/**
+ * Закрывает диалоговое окно
+ * @see showDialog()
+ */
+function closeDialog() {
+
+    $('._dialog').dialog('close');
+}
+
+/**
+ * Полуичть тайтл для сообщения об ошибке
+ * @returns {*|jQuery}
+ */
+function getDialogErrorMessageTitle() {
+
+    return $('._dialog').data('message-error');
+}
---
+++ protected/widgets/dialog/views/index.php
@@ -0,0 +1,7 @@
+<div
+    class="_dialog"
+    data-button-ok="<?= CHtml::encode($messageOk) ?>"
+    data-button-cancel="<?= CHtml::encode($messageCancel) ?>"
+    data-button-close="<?= CHtml::encode($messageClose) ?>"
+    data-message-error="<?= CHtml::encode($messageError) ?>"
+    ></div>
